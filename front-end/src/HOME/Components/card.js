7c51a9a72e1d69a03640e5f750eaee430f0ce70e
Camara Mohamed Salim <m.salimcamara@gmail.com>
2021-11-26 03:13:23 +0100

    Fin du visuel des cartes

--- front-end/src/HOME/Components/card.js
+++ front-end/src/HOME/Components/card.js
@@ -6,13 +6,18 @@ const Card = ({ data }) => {
 
     return(
         <div className="card">
-            <div className="card__title">Anniv de maman</div>
+            <div className="card__title">
+                <h2 className="h2__card">Anniv de maman</h2>
+                <p className="card__date">Lundi 30 novembre 2021</p>
+            </div>
+
             <div className="card__desc">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias aut, repellat ipsum facere voluptate dicta obcaecati deserunt nobis suscipit eaque?
+            Nam quis nulla. Integer malesuada. In in enim a arcu imperdiet malesuada. Sed vel lectus. Donec odio urna, tempus molestie, porttitor ut, iaculis quis, sem. Phasellus rhoncus. Aenean id metus id velit ullamcorper pulvinar. Vestibulum fermentum tortor
             </div>
             <div className="card__tags">
                 <img src={heart} style={{width: '30px', height: '30px'}} />
-                {data}
+                <p className="card_tag"># hello</p>
+                <p className="card_tag"># saucisse</p>
             </div>
         </div>
     )
