b61b3ff3177fdf961cd834209de61cdb78d09028
Camara Mohamed Salim <m.salimcamara@gmail.com>
2021-12-13 01:41:24 +0100

    Ajout de la redirecction avec l'id dans l'url

--- front-end/src/HOME/Pages/weekList.js
+++ front-end/src/HOME/Pages/weekList.js
@@ -2,10 +2,13 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Navigation from "../Components/nav";
 import config from "../../service/config";
+import { useHistory } from "react-router";
 
 
 
 const WeekList = () => {
+
+    const historique = useHistory();
 
     useEffect(() => {
 
@@ -14,28 +17,48 @@ const WeekList = () => {
 
                 const data = memories.data.data;
 
-                for(const semaine of data) {
+                const createElement = async () => {
 
-                    const weekExist = document.getElementById(`${semaine.week}`);
-                    const weeksContainer = document.querySelector('.weeks');
-
-                    if (weekExist == undefined) {
-
-                        const week = document.createElement('div');
-                        week.id = semaine.week;
-                        week.classList.add('weeksOfWeekList');
-                        week.innerHTML = `
-                            <span>
-                                Semaine ${semaine.week} - ${semaine.date.year}
-                            </span>
-                            <span>
-                                <i class="fas fa-arrow-right"></i>
-                            </span>`;
-                        console.log(week);
-
-                        weeksContainer.appendChild(week);
+                    for(const semaine of data) {
+    
+                        const weekExist = document.getElementById(`${semaine.week}`);
+                        const weeksContainer = document.querySelector('.weeks');
+    
+                        if (weekExist == undefined) {
+    
+                            const week = document.createElement('div');
+                            week.id = semaine.week;
+                            week.classList.add('weeksOfWeekList');
+                            week.innerHTML = `
+                                <span>
+                                    Semaine ${semaine.week} - ${semaine.date.year} 
+                                </span>
+                                <span>
+                                    <i class="fas fa-arrow-right"></i>
+                                </span>`;
+    
+                            weeksContainer.appendChild(week);
+                        }
                     }
                 }
+
+                createElement()
+                    .then(() => {
+
+                        const week = document.querySelectorAll('.weeksOfWeekList');
+
+                        week.forEach((item) => {
+                            
+                            item.addEventListener('click', () => {
+                                
+                                const weekId = item.id;
+                                historique.push(`/week#${weekId}`);
+
+                            })
+                        })
+                    })
+                    .catch((err) => console.log(`erreur lors de l'affectation de l'url ` + err))
+
             })
             .catch((err) => console.log('erreur récupération souvenirs ' + err));
 
